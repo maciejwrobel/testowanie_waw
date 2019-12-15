f3b78dbcc82b4dcc6d6a670e6f1e52871154133b
Maciej <wrblmaciej@gmail.com>
2019-12-15 12:37:32 +0100

    asercje cz2

--- 1asercja.py
+++ 1asercja.py
@@ -1,7 +1,47 @@
-def div(a,b):
-    return a/b
+# def div(a,b):
+#     return a/b
+#
+# # if div(10,5)==2:
+# #     print("passed")
+# # else:
+# #     print("failed")
+#
+# # albo
+#
+# assert div(10,5) == 2, "Failed"
+# print("passed")
+# assert div(10,2) == 5, "Failed"
+# print("passed")
 
-if div(10,5)==2:
-    print("passed")
-else:
-    print("failed")
+# def kwadrat_sumy(a,b):
+#     return (a+b)**2
+#
+# assert kwadrat_sumy(1,1) == 4 , "Failed"
+# print("passed")
+# assert kwadrat_sumy(2,2) == 16 , "Failed"
+# print("passed")
+# assert kwadrat_sumy(3,3) == 36 , "Failed"
+# print("passed")
+
+# asercje nie s lużą tylko do testowania !
+# przykład:
+
+# def div(a,b):
+#     try:
+#         return a/b
+#     except ZeroDivisionError:
+#         print("nie można dzielić przez zero")
+# # a jeszcze lepiej i krócej:
+#
+# def div(a,b):
+#     assert b!=0, "Nie można dzielić przez zero"
+#     return a/b
+# div(5,0)
+
+def konkatenacja(a,b):
+    return a+b
+
+assert konkatenacja("Ala ma ", "kota.") == "Ala ma kota.", "failed"
+assert konkatenacja("Ola ma ", "psa.") == "Ola ma psa.", "failed"
+assert konkatenacja("","") == "", "failed"
+
