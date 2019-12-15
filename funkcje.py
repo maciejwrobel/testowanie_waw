1b320a6b74d8e227b5ebca3eaa9ea699f917fa25
Maciej <wrblmaciej@gmail.com>
2019-12-15 15:24:56 +0100

    testy klasy 2 ostro

--- funkcje.py
+++ funkcje.py
@@ -1,3 +1,5 @@
+import math
+
 def dodawanie(x,y):
     return x+y
 
@@ -9,3 +11,10 @@ def kwadrat(x):
 
 def is_palindrome(str1):
     return str1.lower().replace(" ","") == str1[::-1].lower().replace(" ","")
+
+def circle_area(r):
+    if r<0:
+        raise ValueError("the radius cannot be nageative")
+    if type(r) not in [int, float]:
+        raise TypeError("the radius be a non-negative real number")
+    return math.pi *r**2
--- testy.py
+++ testy.py
@@ -1,27 +1,45 @@
-from funkcje import product, dodawanie, kwadrat, is_palindrome
+import funkcje
+import math
+import pytest
+
 
 def test_add():
-    assert dodawanie(1, 2) == 3
-    assert dodawanie(3, -2) == 1
-    assert dodawanie(1.5, 2.5) == 4
+    assert funkcje.dodawanie(1, 2) == 3
+    assert funkcje.dodawanie(3, -2) == 1
+    assert funkcje.dodawanie(1.5, 2.5) == 4
 
 
 def test_product():
-    assert product(1, 1) == 1
-    assert product(2, -2) == -4
-    assert product(1.5, 2) == 3
+    assert funkcje.product(1, 1) == 1
+    assert funkcje.product(2, -2) == -4
+    assert funkcje.product(1.5, 2) == 3
 
 def test_kwadrat():
-    assert kwadrat(1) == 1
-    assert kwadrat(0) == 0
-    assert kwadrat(-2) == 4
+    assert funkcje.kwadrat(1) == 1
+    assert funkcje.kwadrat(0) == 0
+    assert funkcje.kwadrat(-2) == 4
 
 def test_is_palindrome():
-    assert is_palindrome("")
-    assert is_palindrome("Sedes")
-    assert is_palindrome("Kobyła ma mały BOK")
+    assert funkcje.is_palindrome("")
+    assert funkcje.is_palindrome("Sedes")
+    assert funkcje.is_palindrome("Kobyła ma mały BOK")
+
+def test_circle_area():
+    assert funkcje.circle_area(1) == math.pi
+    assert funkcje.circle_area(0) == 0
+    assert funkcje.circle_area(2.1) == math.pi * (2.1**2)
+
+
+def test_values():
+    with pytest.raises(ValueError):
+        funkcje.circle_area(-2)
+
+def test_type():
+    with pytest.raises(TypeError):
+        funkcje.circle_area("asdas")
 
 test_add()
 test_product()
 test_kwadrat()
 test_is_palindrome()
+test_circle_area()
